1c39cf28c3e34b70af505d214d1b9919eeb34858
ChenHaoHSU <chhsu@eda.ee.ntu.edu.tw>
2024-08-27 17:59:07 -0500

    Add operators, mutators, accessors to

--- point2.h
+++ point2.h
@@ -12,14 +12,9 @@ class Point2 {
   // Constructors.
   Point2() : d_({0, 0}) {}
   explicit Point2(T x, T y) : d_({x, y}) {}
-  Point2(Point2& p) {
-    LOG(INFO) << "Copy constructor";
-    d_ = p.d_;
-  }
-  Point2(Point2&& p) {
-    LOG(INFO) << "Move constructor";
-    d_ = p.d_;
-  }
+  explicit Point2(std::initializer_list<T> il) : d_(il) {}
+  Point2(Point2& p) = default;
+  Point2(Point2&& p) = default;
   // Destructors.
   ~Point2() = default;
 
@@ -29,17 +24,40 @@ class Point2 {
   std::array<T, 2>& data() { return d_; }
   const std::array<T, 2>& data() const { return d_; }
 
+  constexpr std::size_t Size() const { return d_.size(); }
+
   // Mutators.
+  void Set(int x, int y) {
+    d_[0] = x;
+    d_[1] = y;
+  }
+  void SetX(int x) { d_[0] = x; }
+  void SetY(int y) { d_[1] = y; }
 
   // Operators.
+  // Operators - Assignment
   Point2& operator=(const Point2& p) {
-    LOG(INFO) << "Operator= ";
     d_ = p.d_;
     return *this;
   }
+  // Operators - Equality
+  bool operator==(const Point2& p) const {
+    return d_[0] == p.d_[0] && d_[1] == p.d_[1];
+  }
+  bool operator!=(const Point2& p) const { return !(*this == p); }
+  // Operators - Inequality
+  bool operator<(const Point2& p) const {
+    return d_[0] != p.d_[0] ? d_[0] < p.d_[0] : d_[1] < p.d_[1];
+  }
+  bool operator>(const Point2& p) const { return p < *this; }
+  bool operator<=(const Point2& p) const { return !(*this > p); }
+  bool operator>=(const Point2& p) const { return !(*this < p); }
+  // Operators - Subscript
+  T& operator[](int i) { return d_[i]; }
+  const T& operator[](int i) const { return d_.at(i); }
 
  private:
-  std::array<T, 2> d_;  // (x, y)
+  std::array<T, 2> d_;  // <x, y>
 };
 
 using Point2_i = Point2<int>;
--- point2_test.cc
+++ point2_test.cc
@@ -27,27 +27,124 @@ TEST(Constructor, Copy) {
 }
 
 TEST(Constructor, Move) {
+  Point2_i p(Point2_i(1, 2));
+
+  EXPECT_EQ(p.x(), 1);
+  EXPECT_EQ(p.y(), 2);
+}
+
+TEST(Accessor, XY) {
+  Point2_i p(1, 2);
+
+  EXPECT_EQ(p.x(), 1);
+  EXPECT_EQ(p.y(), 2);
+}
+
+TEST(Accessor, Data) {
+  Point2_i p(1, 2);
+  std::array<int, 2> d{1, 2};
+
+  EXPECT_EQ(p.data(), d);
+  EXPECT_EQ(p.data().size(), 2);
+}
+
+TEST(Accessor, Size) {
+  Point2_i p;
+
+  EXPECT_EQ(p.Size(), 2);
+}
+
+TEST(Mutator, Set) {
+  Point2_i p(1, 2);
+  p.Set(3, 4);
+
+  EXPECT_EQ(p.x(), 3);
+  EXPECT_EQ(p.y(), 4);
+}
+
+TEST(Mutator, SetX) {
+  Point2_i p(1, 2);
+  p.SetX(3);
+
+  EXPECT_EQ(p.x(), 3);
+  EXPECT_EQ(p.y(), 2);
+}
+
+TEST(Mutator, SetY) {
+  Point2_i p(1, 2);
+  p.SetY(4);
+
+  EXPECT_EQ(p.x(), 1);
+  EXPECT_EQ(p.y(), 4);
+}
+
+TEST(Operator, Assignment) {
   Point2_i p1(1, 2);
-  Point2_i p2(std::move(p1));
+  Point2_i p2;
+  p2 = p1;
 
   EXPECT_EQ(p2.x(), 1);
   EXPECT_EQ(p2.y(), 2);
 }
 
-TEST(Operator, Equal) {
+TEST(Operator, Equality) {
   Point2_i p1(1, 2);
-  Point2_i p2 = p1;
+  Point2_i p2(1, 2);
 
-  EXPECT_EQ(p2.x(), 1);
-  EXPECT_EQ(p2.y(), 2);
+  EXPECT_TRUE(p1 == p2);
+  EXPECT_EQ(p1, p2);
+}
 
-  Point2_i p3;
-  EXPECT_EQ(p3.x(), 0);
-  EXPECT_EQ(p3.y(), 0);
+TEST(Operator, NotEquality) {
+  Point2_i p1(1, 2);
+  Point2_i p2(1, 3);
+  Point2_i p3(2, 2);
+  Point2_i p4(3, 4);
 
-  p3 = p2;
-  EXPECT_EQ(p3.x(), 1);
-  EXPECT_EQ(p3.y(), 2);
+  EXPECT_FALSE(p1 != p1);
+  EXPECT_TRUE(p1 != p2);
+  EXPECT_TRUE(p1 != p3);
+  EXPECT_TRUE(p1 != p4);
+  EXPECT_NE(p1, p2);
+  EXPECT_NE(p1, p3);
+  EXPECT_NE(p1, p4);
+}
+
+TEST(Operator, Inequality1) {
+  Point2_i p1(1, 2);
+  Point2_i p2(2, 1);
+
+  EXPECT_TRUE(p1 < p2);
+  EXPECT_TRUE(p2 > p1);
+  EXPECT_TRUE(p1 <= p2);
+  EXPECT_TRUE(p2 >= p1);
+}
+
+TEST(Operator, Inequality2) {
+  Point2_i p1(1, 2);
+  Point2_i p2(1, 3);
+
+  EXPECT_TRUE(p1 < p2);
+  EXPECT_TRUE(p2 > p1);
+  EXPECT_TRUE(p1 <= p2);
+  EXPECT_TRUE(p2 >= p1);
+}
+
+TEST(Operator, Inequality3) {
+  Point2_i p1(1, 2);
+  Point2_i p2(2, 3);
+
+  EXPECT_TRUE(p1 < p2);
+  EXPECT_TRUE(p2 > p1);
+  EXPECT_TRUE(p1 <= p2);
+  EXPECT_TRUE(p2 >= p1);
+}
+
+TEST(Operator, Subscript) {
+  Point2_i p(1, 2);
+
+  EXPECT_EQ(p[0], 1);
+  EXPECT_EQ(p[1], 2);
 }
 
 }  // namespace moab
