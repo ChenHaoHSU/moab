7d62116905e9e4e1e3579049b7790cc26160e219
ChenHaoHSU <chhsu@eda.ee.ntu.edu.tw>
2024-09-06 01:09:46 -0500

    Add `ring2` class and its test.

---
+++ ring2.h
@@ -0,0 +1,231 @@
+#ifndef MOAB_RING2_H_
+#define MOAB_RING2_H_
+
+#include <cstdint>
+#include <string>
+#include <vector>
+
+#include "absl/strings/str_join.h"
+#include "box2.h"
+#include "operation.h"
+#include "point2.h"
+
+namespace moab {
+
+template <typename T>
+class Ring2 {
+ public:
+  // Type aliases.
+  using base_type = std::vector<Point2<T>>;
+  using coordinate_type = T;
+  using point_type = Point2<T>;
+  using iterator = typename base_type::iterator;
+  using const_iterator = typename base_type::const_iterator;
+  using mutable_iterator = typename base_type::iterator;
+
+  // Constructors.
+  Ring2() = default;
+  explicit Ring2(const std::vector<Point2<T>>& v) : d_(v) {}
+  explicit Ring2(std::vector<Point2<T>>&& v) : d_(std::move(v)) {}
+  Ring2(std::initializer_list<Point2<T>> il) : d_(il) {}
+  Ring2(const Ring2& r) = default;
+  Ring2(Ring2&& r) = default;
+  ~Ring2() = default;
+
+  // Accessors.
+  std::vector<Point2<T>>& Points() { return d_; }
+  const std::vector<Point2<T>>& Points() const { return d_; }
+  size_t Size() const { return d_.size(); }
+  bool Empty() const { return d_.empty(); }
+
+  // Mutators.
+  // Avoid using these methods if possible. It is designed for Boost
+  // polygon/geometry registration.
+  void Clear() { d_.clear(); }
+  void Append(const Point2<T>& p) { d_.push_back(p); }
+  void Resize(std::size_t n) { d_.resize(n); }
+
+  template <typename InputIt>
+  void Assign(InputIt first, InputIt last) {
+    d_.assign(first, last);
+  }
+  void Assign(std::initializer_list<Point2<T>> il) { d_.assign(il); }
+
+  // Iterators.
+  iterator begin() { return d_.begin(); }
+  iterator end() { return d_.end(); }
+  const_iterator begin() const { return d_.begin(); }
+  const_iterator end() const { return d_.end(); }
+  iterator rbegin() { return d_.rbegin(); }
+  iterator rend() { return d_.rend(); }
+  const_iterator rbegin() const { return d_.rbegin(); }
+  const_iterator rend() const { return d_.rend(); }
+
+  // Operators.
+  // Operator - Subscript
+  Point2<T>& operator[](std::size_t i) { return d_[i]; }
+  const Point2<T>& operator[](std::size_t i) const { return d_[i]; }
+  // Operator - Equality
+  bool operator==(const Ring2& r) const { return moab::IsEqual(*this, r); }
+  bool operator!=(const Ring2& r) const { return !(*this == r); }
+
+  // String conversion.
+  template <typename Sink>
+  friend void AbslStringify(Sink& sink, const Ring2& r) {
+    absl::Format(&sink, "(%s)", absl::StrJoin(r.d_, " "));
+  }
+  std::string ToString() const { return absl::StrCat(*this); }
+  friend std::ostream& operator<<(std::ostream& os, const Ring2& r) {
+    os << r.ToString();
+    return os;
+  }
+
+  // Hash.
+  // This is NOT a perfect hash function. Be careful when using this hash
+  // function. It is possible that two equivalent rings with different point
+  // sets have different hash values. They are equal under operator== (the
+  // IsEqual function) but have different hash values.
+  template <typename H>
+  friend H AbslHashValue(H h, const Ring2& r) {
+    h = H::combine(std::move(h), r.d_);
+    return h;
+  }
+
+ private:
+  std::vector<Point2<T>> d_;
+};  // class Ring2
+
+using Ring2_i = Ring2<int>;
+using Ring2_i32 = Ring2<int32_t>;
+using Ring2_i64 = Ring2<int64_t>;
+
+}  // namespace moab
+
+// Boost geometry traits.
+namespace boost::geometry::traits {
+
+template <typename T>
+struct tag<moab::Ring2<T>> {
+  using type = ring_tag;
+};
+
+template <typename T>
+struct clear<moab::Ring2<T>> {
+  static inline void apply(moab::Ring2<T>& r) { r.Clear(); }
+};
+
+template <typename T>
+struct push_back<moab::Ring2<T>> {
+  using point_type = typename moab::Ring2<T>::point_type;
+
+  static inline void apply(moab::Ring2<T>& r, const point_type& p) {
+    r.Append(p);
+  }
+};
+
+template <typename T>
+struct resize<moab::Ring2<T>> {
+  static inline void apply(moab::Ring2<T>& r, std::size_t n) { r.Resize(n); }
+};
+
+template <typename T>
+struct point_order<moab::Ring2<T>> {
+  static const order_selector value = counterclockwise;
+};
+
+template <typename T>
+struct closure<moab::Ring2<T>> {
+  static const closure_selector value = closed;
+};
+
+}  // namespace boost::geometry::traits
+
+// Boost range traits.
+namespace boost {
+
+template <typename T>
+struct range_mutable_iterator<moab::Ring2<T>> {
+  using type = typename moab::Ring2<T>::mutable_iterator;
+};
+
+template <typename T>
+struct range_const_iterator<moab::Ring2<T>> {
+  using type = typename moab::Ring2<T>::const_iterator;
+};
+
+template <typename T>
+struct range_size<moab::Ring2<T>> {
+  using type = std::size_t;
+};
+
+template <typename T>
+inline typename range_mutable_iterator<moab::Ring2<T>>::type range_begin(
+    moab::Ring2<T>& r) {
+  return r.begin();
+}
+
+template <typename T>
+inline typename range_mutable_iterator<moab::Ring2<T>>::type range_end(
+    moab::Ring2<T>& r) {
+  return r.end();
+}
+
+template <typename T>
+inline typename range_const_iterator<moab::Ring2<T>>::type range_begin(
+    const moab::Ring2<T>& r) {
+  return r.begin();
+}
+
+template <typename T>
+inline typename range_const_iterator<moab::Ring2<T>>::type range_end(
+    const moab::Ring2<T>& r) {
+  return r.end();
+}
+
+template <typename T>
+inline typename range_const_iterator<moab::Ring2<T>>::type range_calculate_size(
+    const moab::Ring2<T>& r) {
+  return r.Size();
+}
+
+}  // namespace boost
+
+// Boost polygon traits.
+namespace boost::polygon {
+
+template <typename T>
+struct geometry_concept<moab::Ring2<T>> {
+  using type = polygon_concept;
+};
+
+template <typename T>
+struct polygon_traits<moab::Ring2<T>> {
+  using coordinate_type = typename moab::Ring2<T>::coordinate_type;
+  using point_type = typename moab::Ring2<T>::point_type;
+  using iterator_type = typename moab::Ring2<T>::const_iterator_type;
+
+  static inline iterator_type begin_points(const moab::Ring2<T>& r) {
+    return r.begin();
+  }
+  static inline iterator_type end_points(const moab::Ring2<T>& r) {
+    return r.end();
+  }
+  static inline std::size_t size(const moab::Ring2<T>& r) { return r.Size(); }
+  static inline winding_direction winding(const moab::Ring2<T>& r) {
+    return winding_direction::unknown_winding;
+  }
+};
+
+template <typename T>
+struct polygon_mutable_traits<moab::Ring2<T>> {
+  template <typename InputIt>
+  static inline moab::Ring2<T>& set_points(moab::Ring2<T>& r, InputIt first,
+                                           InputIt last) {
+    r.Assign(begin_points, end_points);
+    return r;
+  }
+};
+
+}  // namespace boost::polygon
+
+#endif  // MOAB_RING2_H_
---
+++ ring2_test.cc
@@ -0,0 +1,270 @@
+#include "ring2.h"
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <string>
+
+#include "absl/hash/hash_testing.h"
+#include "absl/strings/str_format.h"
+#include "box2.h"
+#include "point2.h"
+
+namespace moab {
+
+using ::testing::ElementsAre;
+using ::testing::Pair;
+using ::testing::StrEq;
+
+TEST(Constructor, Default) {
+  Ring2_i r;
+
+  EXPECT_EQ(r.Size(), 0);
+}
+
+TEST(Constructor, VectorCopy) {
+  std::vector<Point2_i> points = {Point2_i(0, 0), Point2_i(2, 0),
+                                  Point2_i(2, 2), Point2_i(0, 2),
+                                  Point2_i(0, 0)};
+  Ring2_i r(points);
+
+  EXPECT_EQ(points.size(), 5);
+  EXPECT_EQ(r.Size(), 5);
+  EXPECT_THAT(r.Points(),
+              ElementsAre(Point2_i(0, 0), Point2_i(2, 0), Point2_i(2, 2),
+                          Point2_i(0, 2), Point2_i(0, 0)));
+}
+
+TEST(Constructor, VectorMove) {
+  std::vector<Point2_i> points = {Point2_i(0, 0), Point2_i(2, 0),
+                                  Point2_i(2, 2), Point2_i(0, 2),
+                                  Point2_i(0, 0)};
+  Ring2_i r(std::move(points));
+
+  EXPECT_EQ(points.size(), 0);
+  EXPECT_EQ(r.Size(), 5);
+  EXPECT_THAT(r.Points(),
+              ElementsAre(Point2_i(0, 0), Point2_i(2, 0), Point2_i(2, 2),
+                          Point2_i(0, 2), Point2_i(0, 0)));
+}
+
+TEST(Constructor, InitializerList) {
+  Ring2_i r = {Point2_i(0, 0), Point2_i(2, 0), Point2_i(2, 2), Point2_i(0, 2),
+               Point2_i(0, 0)};
+
+  EXPECT_EQ(r.Size(), 5);
+  EXPECT_THAT(r.Points(),
+              ElementsAre(Point2_i(0, 0), Point2_i(2, 0), Point2_i(2, 2),
+                          Point2_i(0, 2), Point2_i(0, 0)));
+}
+
+TEST(Constructor, Copy) {
+  Ring2_i r1 = {Point2_i(0, 0), Point2_i(2, 0), Point2_i(2, 2), Point2_i(0, 2),
+                Point2_i(0, 0)};
+  Ring2_i r2(r1);
+
+  EXPECT_EQ(r1.Size(), 5);
+  EXPECT_THAT(r1.Points(),
+              ElementsAre(Point2_i(0, 0), Point2_i(2, 0), Point2_i(2, 2),
+                          Point2_i(0, 2), Point2_i(0, 0)));
+  EXPECT_EQ(r2.Size(), 5);
+  EXPECT_THAT(r2.Points(),
+              ElementsAre(Point2_i(0, 0), Point2_i(2, 0), Point2_i(2, 2),
+                          Point2_i(0, 2), Point2_i(0, 0)));
+}
+
+TEST(Constructor, Move) {
+  Ring2_i r1 = {Point2_i(0, 0), Point2_i(2, 0), Point2_i(2, 2), Point2_i(0, 2),
+                Point2_i(0, 0)};
+  Ring2_i r2(std::move(r1));
+
+  EXPECT_EQ(r1.Size(), 0);
+  EXPECT_THAT(r1.Points(), ElementsAre());
+  EXPECT_EQ(r2.Size(), 5);
+  EXPECT_THAT(r2.Points(),
+              ElementsAre(Point2_i(0, 0), Point2_i(2, 0), Point2_i(2, 2),
+                          Point2_i(0, 2), Point2_i(0, 0)));
+}
+
+TEST(Accessors, Points) {
+  Ring2_i r = {Point2_i(0, 0), Point2_i(2, 0), Point2_i(2, 2), Point2_i(0, 2),
+               Point2_i(0, 0)};
+
+  EXPECT_EQ(r.Size(), 5);
+  EXPECT_THAT(r.Points(),
+              ElementsAre(Point2_i(0, 0), Point2_i(2, 0), Point2_i(2, 2),
+                          Point2_i(0, 2), Point2_i(0, 0)));
+}
+
+TEST(Accessors, PointsConst) {
+  const Ring2_i r = {Point2_i(0, 0), Point2_i(2, 0), Point2_i(2, 2),
+                     Point2_i(0, 2), Point2_i(0, 0)};
+
+  EXPECT_EQ(r.Size(), 5);
+  EXPECT_THAT(r.Points(),
+              ElementsAre(Point2_i(0, 0), Point2_i(2, 0), Point2_i(2, 2),
+                          Point2_i(0, 2), Point2_i(0, 0)));
+}
+
+TEST(Accessors, Size) {
+  Ring2_i r = {Point2_i(0, 0), Point2_i(2, 0), Point2_i(2, 2), Point2_i(0, 2),
+               Point2_i(0, 0)};
+
+  EXPECT_EQ(r.Size(), 5);
+}
+
+TEST(Accessors, Empty1) {
+  Ring2_i r;
+
+  EXPECT_EQ(r.Size(), 0);
+  EXPECT_TRUE(r.Empty());
+}
+
+TEST(Accessors, Empty2) {
+  Ring2_i r = {Point2_i(0, 0), Point2_i(2, 0), Point2_i(2, 2), Point2_i(0, 2),
+               Point2_i(0, 0)};
+
+  EXPECT_EQ(r.Size(), 5);
+  EXPECT_FALSE(r.Empty());
+}
+
+TEST(Mutators, Clear) {
+  Ring2_i r = {Point2_i(0, 0), Point2_i(2, 0), Point2_i(2, 2), Point2_i(0, 2),
+               Point2_i(0, 0)};
+  r.Clear();
+
+  EXPECT_EQ(r.Size(), 0);
+  EXPECT_TRUE(r.Empty());
+}
+
+TEST(Mutators, Append) {
+  Ring2_i r;
+  r.Append(Point2_i(0, 0));
+  r.Append(Point2_i(2, 0));
+  r.Append(Point2_i(2, 2));
+  r.Append(Point2_i(0, 2));
+  r.Append(Point2_i(0, 0));
+
+  EXPECT_EQ(r.Size(), 5);
+  EXPECT_THAT(r.Points(),
+              ElementsAre(Point2_i(0, 0), Point2_i(2, 0), Point2_i(2, 2),
+                          Point2_i(0, 2), Point2_i(0, 0)));
+}
+
+TEST(Mutators, Resize1) {
+  Ring2_i r = {Point2_i(0, 0), Point2_i(2, 0), Point2_i(2, 2), Point2_i(0, 2),
+               Point2_i(0, 0)};
+  r.Resize(0);
+
+  EXPECT_EQ(r.Size(), 0);
+  EXPECT_TRUE(r.Empty());
+  EXPECT_THAT(r.Points(), ElementsAre());
+}
+
+TEST(Mutators, Resize2) {
+  Ring2_i r = {Point2_i(0, 0), Point2_i(2, 0), Point2_i(2, 2), Point2_i(0, 2),
+               Point2_i(0, 0)};
+  r.Resize(10);
+
+  EXPECT_EQ(r.Size(), 10);
+  EXPECT_THAT(r.Points(),
+              ElementsAre(Point2_i(0, 0), Point2_i(2, 0), Point2_i(2, 2),
+                          Point2_i(0, 2), Point2_i(0, 0), Point2_i(0, 0),
+                          Point2_i(0, 0), Point2_i(0, 0), Point2_i(0, 0),
+                          Point2_i(0, 0)));
+}
+
+TEST(Mutators, Resize3) {
+  Ring2_i r = {Point2_i(0, 0), Point2_i(2, 0), Point2_i(2, 2), Point2_i(0, 2),
+               Point2_i(0, 0)};
+  r.Resize(3);
+
+  EXPECT_EQ(r.Size(), 3);
+  EXPECT_THAT(r.Points(),
+              ElementsAre(Point2_i(0, 0), Point2_i(2, 0), Point2_i(2, 2)));
+}
+
+TEST(Mutators, AssignIterator) {
+  Ring2_i r = {Point2_i(0, 0), Point2_i(1, 0), Point2_i(1, 1), Point2_i(0, 1),
+               Point2_i(0, 0)};
+  std::vector<Point2_i> points = {Point2_i(0, 0), Point2_i(1, 0),
+                                  Point2_i(2, 0), Point2_i(2, 2),
+                                  Point2_i(0, 2), Point2_i(0, 0)};
+  r.Assign(points.begin(), points.end());
+
+  EXPECT_EQ(r.Size(), 6);
+  EXPECT_THAT(r.Points(),
+              ElementsAre(Point2_i(0, 0), Point2_i(1, 0), Point2_i(2, 0),
+                          Point2_i(2, 2), Point2_i(0, 2), Point2_i(0, 0)));
+}
+
+TEST(Mutators, AssignInitializer) {
+  Ring2_i r;
+  r.Assign({Point2_i(0, 0), Point2_i(2, 0), Point2_i(2, 2), Point2_i(0, 2),
+            Point2_i(0, 0)});
+
+  EXPECT_EQ(r.Size(), 5);
+  EXPECT_THAT(r.Points(),
+              ElementsAre(Point2_i(0, 0), Point2_i(2, 0), Point2_i(2, 2),
+                          Point2_i(0, 2), Point2_i(0, 0)));
+}
+
+TEST(Operators, SubscriptConst) {
+  const Ring2_i r = {Point2_i(0, 0), Point2_i(2, 0), Point2_i(2, 2),
+                     Point2_i(0, 2), Point2_i(0, 0)};
+
+  EXPECT_EQ(r.Size(), 5);
+  EXPECT_EQ(r[0], Point2_i(0, 0));
+  EXPECT_EQ(r[1], Point2_i(2, 0));
+  EXPECT_EQ(r[2], Point2_i(2, 2));
+  EXPECT_EQ(r[3], Point2_i(0, 2));
+  EXPECT_EQ(r[4], Point2_i(0, 0));
+}
+
+TEST(Operators, Equality) {
+  Ring2_i r1 = {Point2_i(0, 0), Point2_i(2, 0), Point2_i(2, 2), Point2_i(0, 2),
+                Point2_i(0, 0)};
+  Ring2_i r2 = {Point2_i(0, 0), Point2_i(1, 0), Point2_i(2, 0),
+                Point2_i(2, 2), Point2_i(0, 2), Point2_i(0, 0)};
+  Ring2_i r3 = {Point2_i(0, 0), Point2_i(1, 0), Point2_i(2, 0), Point2_i(2, 2),
+                Point2_i(1, 2), Point2_i(0, 2), Point2_i(0, 0)};
+
+  EXPECT_EQ(r1.Size(), 5);
+  EXPECT_EQ(r2.Size(), 6);
+  EXPECT_EQ(r3.Size(), 7);
+  EXPECT_TRUE(r1 == r2);
+  EXPECT_TRUE(r1 == r3);
+  EXPECT_TRUE(r2 == r3);
+  EXPECT_EQ(r1, r2);
+  EXPECT_EQ(r1, r3);
+  EXPECT_EQ(r2, r3);
+}
+
+TEST(StringConversion, ToString) {
+  Ring2_i r = {Point2_i(0, 0), Point2_i(2, 0), Point2_i(2, 2), Point2_i(0, 2),
+               Point2_i(0, 0)};
+
+  EXPECT_THAT(r.ToString(), StrEq("((0 0) (2 0) (2 2) (0 2) (0 0))"));
+}
+
+TEST(StringConversion, SupportsAbslStringify) {
+  Ring2_i r = {Point2_i(0, 0), Point2_i(2, 0), Point2_i(2, 2), Point2_i(0, 2),
+               Point2_i(0, 0)};
+  std::string s = absl::StrFormat("%v", r);
+
+  EXPECT_THAT(s, StrEq("((0 0) (2 0) (2 2) (0 2) (0 0))"));
+}
+
+TEST(Hash, SupportsAbslHash) {
+  EXPECT_TRUE(absl::VerifyTypeImplementsAbslHashCorrectly({
+      Ring2_i(),
+      Ring2_i({Point2_i(0, 0), Point2_i(2, 0), Point2_i(2, 2), Point2_i(0, 2),
+               Point2_i(0, 0)}),
+      Ring2_i({Point2_i(0, 0), Point2_i(2, 0), Point2_i(2, 2), Point2_i(0, 2),
+               Point2_i(0, 0)}),
+      Ring2_i({Point2_i(0, 0), Point2_i(20, 0), Point2_i(20, 20),
+               Point2_i(0, 20), Point2_i(0, 0)}),
+  }));
+}
+
+}  // namespace moab
